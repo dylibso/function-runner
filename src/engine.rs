a86476636307b31f4fa99d19cf81cd931c3e4eff
Benjamin Eckel <bhelx@simst.im>
2023-06-20 17:10:28 -0500

    add message

--- src/engine.rs
+++ src/engine.rs
@@ -53,6 +53,7 @@ pub async fn run(function_path: PathBuf, input: Vec<u8>) -> Result<FunctionRunRe
     let (content_type, data) = MultipartBuilder::new().add_file("wasm", &function_path)?
         .finish()?;
 
+    println!("The wasm code instrumentation is currently in preview, and the API key used in this demo will expire on Sept. 1 2023. Contact support@dylibso.com for your own key.");
     println!("Instrumenting the module first...");
     let resp = ureq::post("https://compiler-preview.dylibso.com/instrument")
       .set("Authorization", "Bearer 48268d3d35a90f8ddfd47ea520a7dba9")
